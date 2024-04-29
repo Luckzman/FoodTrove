accc41fd741a73e418071958cb95625bfdbffae8
Lucky <luckyomokarho@gmail.com>
2024-04-29 11:06:47 +0100

    implement login and signup endpoint

--- backend/models/TagSchema.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const TagSchema = new mongoose.Schema({
-    name: { type: String, unique: true, lowercase: true },
-}, { timestamps: true })
-
-export default mongoose.model('Tag', TagSchema);
